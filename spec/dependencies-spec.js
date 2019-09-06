fc2bca42d5b58aab937ba558b52da0fd05165578
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-06 12:16:46 -0700

    more specs

--- spec/dependencies-spec.js
+++ spec/dependencies-spec.js
@@ -13,8 +13,22 @@ describe('GalacticPerson', function() {
   it('should check if person has age and life expectancy', function() {
     expect(recycledperson.earthAge).toEqual(16);
     expect(recycledperson.lifeExpectancy).toEqual(79);
+    expect(recycledperson.earthAge).not.toEqual(17);
   });
 
+  it('should calculate Mercury age', function() {
+    expect(recycledperson.mercuryAge()).toEqual(3.84);
   });
 
+  it('should calculate Venus age', function() {
+    expect(recycledperson.venusAge()).toEqual(9.92);
+  });
+
+  it('should calculate Mars age', function() {
+    expect(recycledperson.marsAge()).toEqual(30.08);
+  });
+
+  it('should calculate Jupiter age', function() {
+    expect(recycledperson.jupiterAge()).toEqual(189.76);
+  });
 });
--- src/galactic-age-calculator.js
+++ src/galactic-age-calculator.js
@@ -2,3 +2,9 @@ export function GalacticPerson(age, lifeexpectancy,) {
   this.earthAge = age;
   this.lifeExpectancy = lifeexpectancy;
 }
+
+GalacticPerson.prototype. = function (planetName) {
+  const mars =
+  const mercury =
+  if .earthAge
+};
