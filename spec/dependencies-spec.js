d1309e89201711c2af9e59299c59bcd9bf16ec1c
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-06 12:52:14 -0700

    complete back end, all tests passing

--- spec/dependencies-spec.js
+++ spec/dependencies-spec.js
@@ -32,19 +32,23 @@ describe('GalacticPerson', function() {
     expect(recycledperson.jupiterAge()).toEqual(189.76);
   });
 
-  it('should calculate Mercury time left to live' function() {
+  it('should calculate Earth time left to live', function() {
+    expect(recycledperson.timeToLive("Earth")).toEqual(63);
+  });
+
+  it('should calculate Mercury time left to live', function() {
     expect(recycledperson.timeToLive("Mercury")).toEqual(15.12);
   });
 
-  it('should calculate Venus time left to live' function() {
+  it('should calculate Venus time left to live', function() {
     expect(recycledperson.timeToLive("Venus")).toEqual(39.06);
   });
 
-  it('should calculate Mars time left to live' function() {
+  it('should calculate Mars time left to live', function() {
     expect(recycledperson.timeToLive("Mars")).toEqual(118.44);
   });
 
-  it('should calculate Jupiter time left to live' function() {
+  it('should calculate Jupiter time left to live', function() {
     expect(recycledperson.timeToLive("Jupiter")).toEqual(747.18);
   });
 
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { GalacticPerson } from './galactic-age-calculator.js';
+import $ from 'jquery';
+import './styles.css';
+
+$(document).ready(function() {
+  $('').submit(function(event) {
+    event.preventDefault();
+    // var goal = $('#goal').val();
+    // var output = pingPong(goal);
+    // output.forEach(function(element) {
+    //   $('#solution').append("<li>" + element + "</li>");
+    });
+});
