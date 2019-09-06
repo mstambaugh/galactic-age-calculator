d78f6ad0d871c991eb2b04eb2858c8852fd7c68e
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-09-06 09:55:49 -0700

    configuring karma/jasmine

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,56 @@
-{
-  "name": "galactic-age-calculator",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "karma start karma.conf.js",
-    "build": "webpack --mode development",
-    "start": "npm run build; webpack-dev-server --open --mode development",
-    "watch": "webpack --watch",
-    "lint": "eslint src/*.js",
-    "test": "./node_modules/karma/bin/karma start karma.conf.js"
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
-  "repository": {
-    "type": "git"
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
   },
-  "keywords": [],
-  "author": "",
-  "license": "",
-  "bugs": {
-    "url": ""
-  },
-  "homepage": "",
-  "devDependencies": {
-    "babel-core": "^6.26.0",
-    "babel-loader": "^7.1.3",
-    "babel-preset-es2015": "^6.24.1",
-    "clean-webpack-plugin": "^0.1.18",
-    "css-loader": "^0.28.10",
-    "eslint": "^4.18.2",
-    "eslint-loader": "^2.0.0",
-    "html-webpack-plugin": "^3.0.6",
-    "jasmine": "^3.1.0",
-    "jasmine-core": "^2.99.1",
-    "karma": "^2.0.0",
-    "karma-chrome-launcher": "^2.2.0",
-    "karma-cli": "^1.0.1",
-    "karma-jasmine": "^1.1.1",
-    "karma-jasmine-html-reporter": "^0.2.2",
-    "karma-jquery": "^0.2.2",
-    "karma-sourcemap-loader": "^0.3.7",
-    "karma-webpack": "^2.0.13",
-    "style-loader": "^0.20.2",
-    "uglifyjs-webpack-plugin": "^1.2.2",
-    "webpack-cli": "^2.0.9",
-    "webpack-dev-server": "^3.1.0"
-  },
-  "dependencies": {
-    "webpack": "4.19.1",
-    "bootstrap": "^4.0.0",
-    "jquery": "^3.3.1",
-    "popper.js": "^1.14.0"
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'galactic-age-calculator',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
   }
-}
+};
